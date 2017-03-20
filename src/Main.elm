20b3bd556d34daf0a0b6f1e9da9c89320294f871
ryotamac <tennis.suta6@gmail.com>
2017-03-20 19:34:04 +0900

    initailcommit

---
+++ src/Main.elm
@@ -0,0 +1,33 @@
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = None
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        None ->
+            ( model, Cmd.none )
+
+
+main : Program Never {} Msg
+main =
+    program
+        { init = ( {}, Cmd.none )
+        , view = (\m -> Html.text "hello")
+        , update = update
+        , subscriptions = subscriptions
+        }
